9c9cd076a4a0424748102405b7490c5b2902c63b
Madhari Tarunsai <saitarunsai04062002@gmail.com>
2024-12-18 16:08:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+//  create a student obj with deatails of placements using promt and alert
+
+let student=prompt("enter ur name:")
+        let stu=
+        {
+            name:student,
+            companyname:"Genpact",
+            package:"6,79,000",
+            domain:"software_dev",
+            joininddate:"12/02/2025"
+           
+        }
+        alert(`${stu.name} got placed in ${stu.companyname} as a ${stu.domain} as a package of ${stu.package}  and the joining date on ${stu.joininddate}`)
